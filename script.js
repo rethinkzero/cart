8a9dde1cf516146bb080eea958fd76b307363098
rethinkzero <93017797+rethinkzero@users.noreply.github.com>
2025-05-28 12:36:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const cart = document.getElementById("items-section");
+
+const addButtons = document.querySelectorAll("cart-section");
+
+const removeButton = document.getElementById("remove-btn");
+
+addButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const itemName = button.getAttribute("data-item");
+
+    const option = document.createElement("option");
+    option.textContent = itemName;
+    option.value = itemName;
+
+    cart.appendChild(option);
+  });
+});
+
+removeButton.addEventListener("click", () => {
+  const selected = Array.from(cart.selection());  
+
+  selected.forEach(option => {
+    cart.removeChild(selected);
+  });
+});
+
 
